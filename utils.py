4981ecfe4f68e001c9455c11ffa4d36f346a5411
GBowker <guy.bowker@postgrad.manchester.ac.uk>
2023-08-09 15:51:15 +0100

    Model beta approach data with exponential funcs  - Restructure `beta_approach_curve_filter` to output dict of data  - Remove curve keys from `beta-approach_curves.yml`.  -  Restructure `beta_approach_curve_plotter` to plot dict of data

--- utils.py
+++ utils.py
@@ -38,11 +38,42 @@ def curve_filter(lit_data,
                 curve = pd.read_csv(curve['Curve_location'], delimiter=",")
 #                 curve = sort_curve(curve)
                 curve_list.append(curve)
-                Author_list.append(lit_data['Papers'][paper]['Authors'])
+                Author_list.append(lit_data['Papers'][paper]['Authors'][0])
                 Paper_list.append(lit_data['Papers'][paper]['Title'])
                 no_curves +=1
     return curve_list, no_curves, Paper_list, Author_list
-    
+
+
+def beta_approach_curve_filter(lit_data,
+                              authors_list=[],
+                              year_of_publication=[],
+                              heating_rate='any', 
+                              Strain_rate='any',  
+                              heater='any'
+                             ):
+    filtered_data = {}
+    filtered_data['Papers'] = {}
+    for paper in lit_data['Papers'].keys():
+        filtered_data['Papers'][paper] = {}
+        
+        authors_bool = lit_data['Papers'][paper]['Authors'][0] in authors_list or authors_list==[]
+        year_of_publication_bool = lit_data['Papers'][paper]['year_of_publication'] in year_of_publication or year_of_publication==[]
+        
+        filtered_data['Papers'][paper]['Curves'] = []
+        for curve_num, curve in enumerate(lit_data['Papers'][paper]['Curves']):
+            
+            heating_rate_bool = curve['heating_rate'] == heating_rate or heating_rate=='any' or heating_rate=='check'
+            Strain_rate_bool = curve['Strain_rate_s-1'] == Strain_rate or Strain_rate =='any' or Strain_rate=='check'
+            heater_bool = curve['In_situ_heating'] == heater or heater == 'any' or heater=='check'
+            
+            if authors_bool & year_of_publication_bool & heating_rate_bool & Strain_rate_bool & heater_bool:
+                filtered_data['Papers'][paper]['Curves'].append( lit_data['Papers'][paper]['Curves'][curve_num]['data'] )
+                filtered_data['Papers'][paper]['Title'] = lit_data['Papers'][paper]['Title']
+                filtered_data['Papers'][paper]['Authors'] = lit_data['Papers'][paper]['Authors']
+                filtered_data['Papers'][paper]['year_of_publication'] = lit_data['Papers'][paper]['year_of_publication']
+                
+    return filtered_data
+
 
 def curve_filter_pd(Temperature_C='any', 
                  Strain_rate='any', 
@@ -79,16 +110,17 @@ def flow_curve_plotter(curve_list, fig_no, Authors, Temperature_C='any', Strain_
     plt.xlabel('strain')
     plt.ylabel('Stress/MPa')
     plt.legend(bbox_to_anchor=[1, 1])
-    
-def beta_approach_curve_plotter(curve_list, Authors, microstructure='any', heater='any'):
-    marker_list = ['s', '^', 'h', '*', 'X', '+', 'o', '.', 'P']
-    
-    for n, curve in enumerate(curve_list):
-        plt.plot(curve.iloc[:,0], curve.iloc[:,1], label=Authors[n],
-                 marker=marker_list[n], linestyle='none', color='k'
-                )
-    plt.title('Beta approach curves of Ti64')
 
+
+def beta_approach_curve_plotter(filtered_data):
+    marker_list = ['s', '^', 'h', 'X', '*', '+', 'o', '.', 'P']
+    
+    for p, paper in enumerate(filtered_data['Papers']):
+        for c, curve in enumerate(filtered_data['Papers'][paper]['Curves']):
+            plt.plot(curve.iloc[:,0], curve.iloc[:,1], label=f"{paper}",
+                     marker=marker_list[p], linestyle='none', color='k'
+                    )
+    plt.title('Beta approach curves of Ti64')
     plt.xlim([None,1000])
     plt.xlabel(f"Temperature [C]")
     plt.ylim([0,100])
