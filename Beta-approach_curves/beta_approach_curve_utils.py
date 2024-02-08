576e34d414b428a7ba178e5ebfb7c15a64944d52
GBowker <guy.bowker@postgrad.manchester.ac.uk>
2024-02-08 16:35:40 +0000

    Make Beta-approach work same as flow curves

--- Beta-approach_curves/beta_approach_curve_utils.py
+++ Beta-approach_curves/beta_approach_curve_utils.py
@@ -1,9 +1,39 @@
 import yaml
 from yaml.loader import FullLoader
 from yaml.loader import SafeLoader
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 import pandas as pd
+
+
+def load_yml(alloy, yml_file='beta-approach_curves.yml'):
+    
+    database = yaml.load(open('beta-approach_curves.yml'), Loader=SafeLoader)[alloy]
+    # LOAD EXPT DATA
+    for expt in database['Experiments'].keys():
+        for curve_no, curve_metadata in enumerate(database['Experiments'][expt]['Curves']):
+            path_to_data = os.path.abspath(database['Experiments'][expt]['Curves'][curve_no]['Curve_location'])
+            expt_curve_data = pd.read_csv(path_to_data,
+                                         delimiter=",")
+            database['Experiments'][expt]['Curves'][curve_no]['data'] = expt_curve_data
+            database['Experiments'][expt]['Curves'][curve_no]['dataset_key'] = expt # include dataset key in curve
+
+    # LOAD LIT DATA
+#     try:
+    for paper in database['Papers'].keys():
+        for curve_no, curve_metadata in enumerate(database['Papers'][paper]['Curves']):
+            path_to_data = os.path.abspath(database['Papers'][paper]['Curves'][curve_no]['Curve_location'])
+            lit_curve_data = pd.read_csv(path_to_data,
+                                     delimiter=",")
+            database['Papers'][paper]['Curves'][curve_no]['data'] = lit_curve_data
+            database['Papers'][paper]['Curves'][curve_no]['dataset_key'] = paper # include dataset key in curve
+#     except:
+#         database['Papers'] = None
+        
+    print(f"\nExperiments: \n{database['Experiments'].keys()}")
+    print(f"\nPapers: \n{database['Papers'].keys()}")
+    return database
 
 
 def sort_curve(curve):
@@ -14,90 +44,74 @@ def sort_curve(curve):
         sorted_curve[i,1]= curve[ind[i,0],1]
     return sorted_curve
 
-def expt_data_filter():
-    # Experimental data filter
-    for expt_key in database['Experiments'].keys():
-        authors_bool = database['Experiments'][expt_key]['Authors'][0] in authors_list or authors_list==[]
-        year_of_publication_bool = database['Experiments'][expt_key]['year_of_publication'] in year_of_publication or year_of_publication==[]
-        # exclusion bool true if in list
-        exclusion_bool = database['Experiments'][expt_key]['Authors'][0] in exclsuions or database['Experiments'][expt_key]['year_of_publication'] in exclusions
+
+def include_dataset(curve, chosen_parameters):
+
+    # USER DEFINED PARAMS
+    chosen_heatrate =       chosen_parameters['heating_rate']
+    chosen_strainrate =     chosen_parameters['Strain_rate_s-1']
+    chosen_heater =         chosen_parameters['In_situ_heating']
+    exclusions =            chosen_parameters['exclusions']
+    
+    # EACH BOOL TRUE IF MATCHING USER CHOICE OR 'ANY' CHOSEN
+    heatrate_bool       = curve['heating_rate']==chosen_heatrate              or chosen_heatrate=='any'
+    strainrate_bool     = curve['Strain_rate_s-1']==chosen_strainrate         or chosen_strainrate=='any'
+    heattype_bool       = curve['In_situ_heating']==chosen_heater             or chosen_heater=='any'
+    
+    # EXCLUDE IF KEY, TITLE, OR AUTHORS ARE IN EXCLUSIONS LIST:
+    exclude = curve['dataset_key'] in exclusions
+    
+    # IF ALL TRUE INCLUDE DATASET
+    if heatrate_bool & strainrate_bool & heattype_bool:
+        if exclude:
+            print(f"{curve['dataset_key']} IN EXCLUSIONS LIST.")
+            include_bool = False
+        else:
+            include_bool = True
+    else:
+        include_bool = False
+    return include_bool
+
+
+def curve_filter(database, chosen_parameters):
+    print(f"Chosen heating rate:{chosen_parameters['heating_rate']}\n") # DEBUG: Inform user of chosen params
+    
+    # initialise filtered database as empty dict of dicts:
+    filtered_database = {'Experiments' : {  },
+                         'Papers' : {  }  }
+    
+    for datasource in ['Experiments', 'Papers']: # ['Experiments', 'Papers'] # DEBUG: JUST USE PAPERS FOR NOW...
+        for dataset_key in database[datasource]: # idividual expt/paper
+            curve_list = [] # initialise empty curve list on looking at new dataset...
+            for curve_num, curve in enumerate(database[datasource][dataset_key]['Curves']):
+                if include_dataset(curve, chosen_parameters): # If include func returns true:
+                    print(f"Including {dataset_key}") # DEBUG
+                    curve_list.append(curve)
+                    # only add dataset_key to dict if curve contains chosen params
+                    filtered_database[datasource][dataset_key] = {
+                                                                  'Title' : database[datasource][dataset_key]['Title'],
+                                                                  'Authors' : database[datasource][dataset_key]['Authors'],
+                                                                  'year_of_publication' : database[datasource][dataset_key]['Title'],
+                                                                  'Curves': curve_list
+                                                                 }
+                else:
+#                     print(f"Excluding {dataset_key} curve {curve_no}") # DEBUG
+                    pass # if not included, move on...
         
-        if authors_bool & year_of_publication_bool:
-            for curve_num, curve in enumerate(database['Experiments'][expt_key]['Curves']):
-                # must sort curve by T for fitting to work:
-                sorted_curve = database['Experiments'][expt_key]['Curves'][curve_num]['data'].sort_values(by=['T'])
-            
-                heating_rate_bool = curve['heating_rate'] == heating_rate or heating_rate=='any' or heating_rate=='check'
-                Strain_rate_bool = curve['Strain_rate_s-1'] == Strain_rate or Strain_rate =='any' or Strain_rate=='check'
-                heater_bool = curve['In_situ_heating'] == heater or heater == 'any' or heater=='check'
-            
-                if heating_rate_bool & Strain_rate_bool & heater_bool:
-                    filtered_data['Experiments'][expt_key]['Curves'].append( sorted_curve )
-                    filtered_data['Experiments'][expt_key]['Title'] = database['Experiments'][expt_key]['Title']
-                    filtered_data['Experiments'][expt_key]['Authors'] = database['Experiments'][expt_key]['Authors']
-                    filtered_data['Experiments'][expt_key]['year_of_publication'] = database['Experiments'][expt_key]['year_of_publication']
-
-    return filtered_data
+    return filtered_database
 
 
-def lit_data_filter(database):
-    for paper_key in database['Papers'].keys():
-        authors_bool = database['Papers'][paper_key]['Authors'][0] in authors_list or authors_list==[]
-        year_of_publication_bool = database['Papers'][paper_key]['year_of_publication'] in year_of_publication or year_of_publication==[]
-        # exclusion bool true if any in list # NOT YET IMPLEMENTED!
-        exclusion_bool = database['Experiments'][expt_key]['Authors'][0] in exclsuions or database['Experiments'][expt_key]['year_of_publication'] in exclusions
-        
-        if authors_bool & year_of_publication_bool:
-            for curve_num, curve in enumerate(database['Papers'][paper_key]['Curves']):
-                # must sort curve by T for fitting to work:
-                sorted_curve = database['Papers'][paper_key]['Curves'][curve_num]['data'].sort_values(by=['T'])
-            
-                heating_rate_bool = curve['heating_rate'] == heating_rate or heating_rate=='any' or heating_rate=='check'
-                Strain_rate_bool = curve['Strain_rate_s-1'] == Strain_rate or Strain_rate =='any' or Strain_rate=='check'
-                heater_bool = curve['In_situ_heating'] == heater or heater == 'any' or heater=='check'
-            
-                if heating_rate_bool & Strain_rate_bool & heater_bool:
-                    filtered_data['Papers'][paper_key]['Curves'].append( sorted_curve )
-                    filtered_data['Papers'][paper_key]['Title'] = database['Papers'][paper_key]['Title']
-                    filtered_data['Papers'][paper_key]['Authors'] = database['Papers'][paper_key]['Authors']
-                    filtered_data['Papers'][paper_key]['year_of_publication'] = database['Papers'][paper_key]['year_of_publication']
-                
-    return filtered_data
-
-
-def curve_filter(database, keys_list=[], authors_list=[], year_of_publication=[], exclusions=[],
-                 heating_rate='any', Strain_rate='any', heater='any',
-                 ):
-    filtered_data = {}
-    filtered_data['Papers'] = {}
-    filtered_data['Papers'][paper_key] = {'Curves':[]}
-    
-    filtered_data['Experiments'] = {}
-    filtered_data['Experiments'][expt_key] = {'Curves':[]}
-    
-    # Experimental data filter
-    filtered_data = expt_data_filter(database)
-    
-    # Literature data filter
-    filtered_data = lit_data_filter(database)
-    
-    return filtered_data
-
-
-def curve_plotter(filtered_data):
+def curve_plotter(database):
     marker_list = ['s', '^', 'h', 'X', '*', '+', 'o', '.', 'P']
     
-    for e, expt in enumerate(filtered_data['Experiments']):
-        for c, curve in enumerate(filtered_data['Experiments'][expt]['Curves']):
-            plt.plot(curve.iloc[:,0], curve.iloc[:,1], label=f"{expt}",
-                     marker=marker_list[e], linestyle='none', color='k'
-                    )
-    
-    for p, paper in enumerate(filtered_data['Papers']):
-        for c, curve in enumerate(filtered_data['Papers'][paper]['Curves']):
-            plt.plot(curve.iloc[:,0], curve.iloc[:,1], label=f"{paper}",
-                     marker=marker_list[p], linestyle='none', color='k'
-                    )
+    for datasource in ['Papers']: # ['Experiments', 'Papers'] # DEBUG: JUST USE PAPERS FOR NOW...
+        for p, dataset_key in enumerate(database[datasource]): # idividual expt/paper
+            for curve in database[datasource][dataset_key]['Curves']:
+                plt.plot(curve['data'].iloc[:,0], curve['data'].iloc[:,1], label=dataset_key,
+                         marker=marker_list[p], linestyle='none', color='k'
+                        )
+
+            
     plt.title('Beta approach curves of Ti64')
     plt.xlim([None,1000])
     plt.xlabel(f"Temperature [C]")
